1a45ac3b8581287705f5b63ee5396a5386763392
Rahul <rahulghosh@duck.com>
2023-04-26 16:04:00 +0530

    Initial commit to AUTH SERVER

---
+++ src/main/java/com/auth/server/authcode/AuthcodeApplication.java
@@ -0,0 +1,13 @@
+package com.auth.server.authcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/server/authcode/AuthcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.server.authcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
